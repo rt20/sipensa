673a957fa1f0747f8f6364acaf6c98bbd08b6a02
Hilman <kaploks@gmail.com>
2021-03-13 13:29:49 +0700

    Revert "Versi 0.1.1"
    This reverts commit 8ba06c13a81016032f49f2f3cbc625a04a46f846.

--- resources/views/lte/master.blade.php
+++ resources/views/lte/master.blade.php
@@ -3,7 +3,6 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
   <title>SIPENSA | Sistem Informasi Pemeriksaan Sarana Pangan</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
