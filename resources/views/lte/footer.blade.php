ee6f6a38bd9903917563c27c71fd2a9150987fd3
Hilman <kaploks@gmail.com>
2021-03-13 13:36:28 +0700

    Version 0.1.2
    Fix Popup Modal

--- resources/views/lte/footer.blade.php
+++ resources/views/lte/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
     
-      <b>Version</b> 0.1.0
+      <b>Version</b> 0.1.1
     </div>
     <strong>Hak Cipta &copy; 2021 <a>Direktorat Pengawasan Peredaran Pangan Olahan | BPOM</a>. </strong> 
   </footer>
